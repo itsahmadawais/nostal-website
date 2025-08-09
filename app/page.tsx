c93dd5107e8275fb9234ad692fc469a8a7b3f65f
Awais Ahmad <awaisahmad1276@gmail.com>
2025-08-09 09:02:24 +0500

    👌 IMPROVE:

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ const ComingSoonPage = () => {
         <h1 className="text-[#a6a6a6] font-sans text-4xl md:text-6xl mb-6">Coming soon...</h1>
 
         {/* Paragraph */}
-        <p className="text-white text-md md:text-xl font-poppins max-w-xl text-white/80 tracking-normal">
+        <p className="text-white text-sm md:text-xl font-poppins max-w-xl text-white/80 tracking-normal">
           Join Nostal — the first Web3-powered social app where your posts pay. Drop content,
           bring your crew, and earn rewards while you scroll.
         </p>
